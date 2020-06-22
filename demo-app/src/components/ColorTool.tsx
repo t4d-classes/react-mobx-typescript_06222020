b5d30fc77f36bf526dfe5d14e97edd06e5bc3989
Eric Greene <eric@t4d.io>
2020-06-22 16:30:03 -0400

    add color list code

--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import { Color } from '../models/color';
+
 export const ColorTool = () => {
+
+  const colors: Color[] = [
+    { id: 1, name: 'red', hexcode: 'FF0000' }, 
+    { id: 2, name: 'black', hexcode: '000000' }, 
+    { id: 3, name: 'blue', hexcode: '0000FF' }, 
+    { id: 4, name: 'periwinkle', hexcode: 'CCCCFF' }, 
+  ];
 
   return (
     <>
@@ -8,10 +17,9 @@ export const ColorTool = () => {
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>red</li>
-        <li>black</li>
-        <li>blue</li>
-        <li>periwinkle</li>
+        {colors.map(color => <li key={color.id}>
+          {color.name}
+        </li>)}
       </ul>
     </>
   );
