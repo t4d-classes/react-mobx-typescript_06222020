b35fd71822c534f55287feea1e59f30600cc581c
Eric Greene <eric@t4d.io>
2020-06-25 13:31:27 -0400

    add exercise 14

---
+++ demo-app/exercises/exercise-14.md
@@ -0,0 +1,7 @@
+# Exercise 14
+
+1. Add a new button next the divide but with a label of "Clear". When the button is clicked clear the history, set the result and the input field to 0.
+
+2. Place a button next to each item in the history, when the button is clicked, delete the entry from the history list. Do not worry about updating the result.
+
+3. Ensure it works!
--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -5,29 +5,64 @@ import { useObserver } from 'mobx-react-lite';
 
 import 'mobx-react-lite/batchingForReactDom';
 
+// interface HistoryEntry {
+//   id: number;
+//   opName: string;
+//   opValue: number;
+// }
+
+type HistoryEntry = {
+  id: number,
+  opName: string,
+  opValue: number,
+};
+
 class CalcToolStore {
 
   @observable
   result = 0;
 
+  @observable
+  history: HistoryEntry[] = [];
+
   @action.bound
   add(val: number) {
     this.result += val;
+    this.history.push({
+      opName: 'Add',
+      opValue: val,
+      id: Math.max(...this.history.map(c => c.id), 0) + 1,
+    });
   }
 
   @action.bound
   subtract(val: number) {
     this.result -= val;
+    this.history.push({
+      opName: 'Subtract',
+      opValue: val,
+      id: Math.max(...this.history.map(c => c.id), 0) + 1,
+    });
   }
 
   @action.bound
   multiply(val: number) {
     this.result *= val;
+    this.history.push({
+      opName: 'Multiply',
+      opValue: val,
+      id: Math.max(...this.history.map(c => c.id), 0) + 1,
+    });
   }
 
   @action.bound
   divide(val: number) {
     this.result /= val;
+    this.history.push({
+      opName: 'Divide',
+      opValue: val,
+      id: Math.max(...this.history.map(c => c.id), 0) + 1,
+    });
   }
 
 }
@@ -63,6 +98,12 @@ const CalcTool: FC<CalcToolProps> = ({ store }) => {
             /
           </button>
         </div>
+        <ul>
+          {store.history.map(entry =>
+            <li key={entry.id}>
+              {entry.opName} - {entry.opValue}
+            </li>)}
+        </ul>
       </form>
     );
 
