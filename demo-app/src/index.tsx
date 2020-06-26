5e3026923f2d70aa5cc4ae087418ccca17673843
Eric Greene <eric@t4d.io>
2020-06-26 12:47:46 -0400

    add my fetch

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import { CarToolStore } from './stores/CarToolStore';
+// import { CarToolStore } from './stores/CarToolStore';
 
-import { CarTool } from './components/CarTool';
+// import { CarTool } from './components/CarTool';
 
-const store = new CarToolStore();
+// const store = new CarToolStore();
 
-ReactDOM.render(
-  <CarTool store={store} />,
-  document.querySelector('#root'),
+// ReactDOM.render(
+//   <CarTool store={store} />,
+//   document.querySelector('#root'),
 
-);
+// );
 
